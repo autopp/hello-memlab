d6f996998d3150f079db069ca09d9ee0633cae44
autopp <autopp.inc@gmail.com>
2022-10-05 22:42:15 +0900

    feat: add typescript example

---
+++ google-maps-ts/google-maps.ts
@@ -0,0 +1,20 @@
+import type { Page } from "puppeteer"
+
+// initial page load url: Google Maps
+function url() {
+  return 'https://www.google.com/maps/@37.386427,-122.0428214,11z';
+}
+
+// action where we want to detect memory leaks: click the Hotels button
+async function action(page: Page) {
+  // puppeteer page API
+  await page.click('button[aria-label="ホテル"]');
+}
+
+// action where we want to go back to the step before: click clear search
+async function back(page: Page) {
+  // puppeteer page API
+  await page.click('[aria-label="検索をクリア"]');
+}
+
+module.exports = {action, back, url};
